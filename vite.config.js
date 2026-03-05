2a063b7ac7489c369c63d66c75f1c9362e8e75f8
Yann Karera <y_karera@outlook.be>
2026-03-05 13:53:40 +0100

    Last Commit

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  root: './src',
   publicDir: './public',
-  build: {
-    outDir: '../dist',
-  },
-  server: {
-    port: 3000,
-    open: true,
-    historyApiFallback: true,
-  },
   plugins: [react()],
 })
